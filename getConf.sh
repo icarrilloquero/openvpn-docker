8fcd0e02d765aec01f8ab5c156be9b8903801a77
icarrilloquero <icarrilloquero@gmail.com>
2019-05-06 13:06:23 +0200

    Update getConf.sh
    Automatically replace port

--- getConf.sh
+++ getConf.sh
@@ -10,4 +10,5 @@ CONTAINER=ovpn_vpn_1
 docker exec $CONTAINER rm /etc/openvpn/pki/reqs/$CLIENT_NAME.req
 docker exec $CONTAINER rm /etc/openvpn/pki/private/$CLIENT_NAME.key
 docker run -v $OVPN_DATA:/etc/openvpn --rm -it $IMAGE easyrsa build-client-full $CLIENT_NAME nopass && \
-docker run -v $OVPN_DATA:/etc/openvpn --rm $IMAGE ovpn_getclient $CLIENT_NAME > $CLIENT_NAME.ovpn
+docker run -v $OVPN_DATA:/etc/openvpn --rm $IMAGE ovpn_getclient $CLIENT_NAME > $CLIENT_NAME.ovpn && \
+sed --expression="s/1194 $PROTOCOL/$VPN_PORT $PROTOCOL/g" -i $CLIENT_NAME.ovpn
