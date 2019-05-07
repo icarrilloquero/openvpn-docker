b8e19cb140a345ee7fa6bf3e6fa2d84cb99e699b
icarrilloquero <icarrilloquero@gmail.com>
2019-05-07 13:37:17 +0200

    Update getConf.sh

--- getConf.sh
+++ getConf.sh
@@ -11,4 +11,5 @@ docker exec $CONTAINER rm /etc/openvpn/pki/reqs/$CLIENT_NAME.req
 docker exec $CONTAINER rm /etc/openvpn/pki/private/$CLIENT_NAME.key
 docker run -v $OVPN_DATA:/etc/openvpn --rm -it $IMAGE easyrsa build-client-full $CLIENT_NAME nopass && \
 docker run -v $OVPN_DATA:/etc/openvpn --rm -t $IMAGE ovpn_otp_user $CLIENT_NAME && \
-docker run -v $OVPN_DATA:/etc/openvpn --rm $IMAGE ovpn_getclient $CLIENT_NAME > $CLIENT_NAME.ovpn
+docker run -v $OVPN_DATA:/etc/openvpn --rm $IMAGE ovpn_getclient $CLIENT_NAME > $CLIENT_NAME.ovpn && \
+sed --expression="s/1194 $PROTOCOL/$VPN_PORT $PROTOCOL/g" -i $CLIENT_NAME.ovpn
